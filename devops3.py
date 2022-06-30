dde63405fed7b428d88e2502dda625fec1c25733
Khaled Dallah <khaled.dallah0@gmail.com>
2022-07-01 01:15:21 +0300

    List all repositories | bootstrap & python(flask)

--- devops3.py
+++ devops3.py
@@ -1,11 +1,18 @@
-
-
 import requests
 import json
+
+
+from flask import Flask, render_template, request, redirect, url_for, make_response
+import socket, random
+
 
 accessToken = "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"
 endpoint = "https://api.github.com/graphql"
 auth_header = {'Authorization': 'Token ' + accessToken}
+server_ip = "0.0.0.0"
+app = Flask(__name__) 
+
+
 
 def GenerateQuery(name, after_cursor=None):
     after_cursor = str("\""+after_cursor+"\"") if after_cursor else "null"
@@ -60,9 +67,24 @@ def GetRepositories(name):
 
 
 def main():
-    results = GetRepositories("facebook")
-    print(results)
-    print("length: "+str(len(results)))
+    app.config["CACHE_TYPE"] = "null"
+    app.run(debug=True, host='0.0.0.0', port=8001) 
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', server_ip=server_ip)
+
+
+@app.route('/test', methods=['POST'])
+def reroute():
+    name = request.json['value']
+    print(name)
+    data = GetRepositories(name)
+    print(data)
+    return {"data":data}
+    
 
 
 if __name__ == "__main__":
